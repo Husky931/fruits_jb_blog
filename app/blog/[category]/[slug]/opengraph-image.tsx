28aa2ed95e2ad145fdc95e0d10c46426bca4987c
Xz33M Hask <pecevgligor@protonmail.com>
2023-08-11 21:09:35 +0800

    add opengraph-tsx

---
+++ app/blog/[category]/[slug]/opengraph-image.tsx
@@ -0,0 +1,29 @@
+import { ImageResponse } from "next/server"
+import { fetchAPI } from "../../utils/fetch-api"
+
+export const size = {
+    width: 1200,
+    height: 630
+}
+
+export const contentType = "image/png"
+
+export default async function og({ params }: { params: { slug: string } }) {
+    async function getMetaData(slug: string) {
+        const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN
+        const path = `/articles`
+        const urlParamsObject = {
+            filters: { slug },
+            populate: { seo: { populate: "*" } }
+        }
+        const options = { headers: { Authorization: `Bearer ${token}` } }
+        const response = await fetchAPI(path, urlParamsObject, options)
+        return response.data
+    }
+
+    const meta = await getMetaData(params.slug)
+    const metadata = meta[0].attributes.seo
+    const ogImageURL = metadata.shareImage.data.attributes.url
+
+    return new ImageResponse(<img src={ogImageURL} />, size)
+}
--- app/blog/[category]/[slug]/page.tsx
+++ app/blog/[category]/[slug]/page.tsx
@@ -38,21 +38,10 @@ export async function generateMetadata({
 }): Promise<Metadata> {
     const meta = await getMetaData(params.slug)
     const metadata = meta[0].attributes.seo
-    // const ogImage = metadata.shareImage.data.attributes.url
-
-    // console.log(metadata, "i am metadata")
-    // console.log(metadata.shareImage, "i am ogImage")
-    // console.log(ogImage, "i am ogImage")
 
     return {
         title: metadata.metaTitle,
         description: metadata.metaDescription
-        // openGraph: {
-        //     type: "website",
-        //     url: ogImage,
-        //     siteName: "Fruit picking jobs",
-        //     description: "daily friuit picking job posts",
-        // }
     }
 }
 
