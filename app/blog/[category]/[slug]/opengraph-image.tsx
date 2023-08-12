c8212dcef29b630d48e0c5e18c276af5437fe66f
Xz33M Hask <pecevgligor@protonmail.com>
2023-08-12 11:14:11 +0800

    test

--- app/blog/[category]/[slug]/opengraph-image.tsx
+++ app/blog/[category]/[slug]/opengraph-image.tsx
@@ -14,5 +14,29 @@ export const contentType = "image/jpeg"
 export default async function Image({ params }: { params: { slug: string } }) {
     const imageUrl = imgSrc.src
 
-    return new ImageResponse(<img src={imageUrl} />, size)
+    try {
+        const imageUrl = imgSrc.src
+
+        return new ImageResponse(
+            (
+                <div style="height: 100%; width: 100%; display: flex; textAlign: center; alignItems: center; justifyContent: center;">
+                    <img
+                        alt="My Image"
+                        height="630"
+                        src="${imageUrl}"
+                        width="1200"
+                    />
+                </div>
+            ),
+            {
+                width: 1200,
+                height: 630
+            }
+        )
+    } catch (e: any) {
+        console.log(`${e.message}`)
+        return new Response(`Failed to generate the image`, {
+            status: 500
+        })
+    }
 }
