b8cccb4da99d717d7b8a428d1e406667fd17c42c
Xz33M Hask <pecevgligor@protonmail.com>
2023-08-12 11:35:35 +0800

    test new

--- app/blog/[category]/[slug]/opengraph-image.tsx
+++ app/blog/[category]/[slug]/opengraph-image.tsx
@@ -13,44 +13,34 @@ export const alt = ""
 export const contentType = "image/jpeg"
 
 export default async function Image({ params }: { params: { slug: string } }) {
-    const fontData = await fetch(
-        new URL("./fonts/Inter-Medium.ttf", import.meta.url)
-    ).then((res) => res.arrayBuffer())
-    try {
-        return new ImageResponse(
-            (
-                <div
-                    style={{
-                        display: "flex",
-                        fontSize: 60,
-                        color: "black",
-                        background: "#f6f6f6",
-                        width: "100%",
-                        height: "100%",
-                        paddingTop: 50,
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}
-                >
-                    <img
-                        alt="My Image"
-                        height="630"
-                        //@ts-ignore
-                        src={new url(imgSrc)}
-                        width="1200"
-                    />
-                </div>
-            ),
-            {
-                width: 1200,
-                height: 630
-            }
-        )
-    } catch (e: any) {
-        console.log(`${e.message}`)
-        return new Response(`Failed to generate the image`, {
-            status: 500
-        })
-    }
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    display: "flex",
+                    fontSize: 60,
+                    color: "black",
+                    background: "#f6f6f6",
+                    width: "100%",
+                    height: "100%",
+                    paddingTop: 50,
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                <img
+                    alt="My Image"
+                    height="630"
+                    //@ts-ignore
+                    src={new url(imgSrc)}
+                    width="1200"
+                />
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 630
+        }
+    )
 }
