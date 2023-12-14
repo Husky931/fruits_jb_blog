917b1dc95e11007f09af50afc3c4181ce5fa410f
Xz33M Hask <pecevgligor@protonmail.com>
2023-12-14 12:12:57 +0800

    update

--- app/(employers)/employer/components/HeroImage.tsx
+++ app/(employers)/employer/components/HeroImage.tsx
@@ -5,8 +5,8 @@ export default function HeroImage() {
     return (
         <header className="relative w-full h-[400px] md:h-[400px] xl:h-[500px] 2xl:h-[700px] bg-no-repeat bg-cover bg-top bg-[url('/employers-mob.jpg')] md:bg-[url('/employers.jpg')]">
             <div className="z-10 text-center p-4 flex flex-col justify-center items-center w-auto h-full md:items-start md:justify-center md:ml-4">
-                <h1 className="text-[40px] lg:text-[60px] font-bold !text-yellow-300">
-                    Make your New harvest hire
+                <h1 className="text-[40px] lg:text-[60px] text-white font-bold">
+                    Get your New hire
                 </h1>
                 <p className="text-[20px] mt-2 text-white">
                     Post a job for free and find the right candidate
